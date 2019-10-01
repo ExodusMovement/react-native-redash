809a57a504dc1ea7300528b94f317ee58a8a0313
Damian Sznajder <damsznajder@gmail.com>
2019-10-01 18:51:08 +0200

    feat(📦): Add toggle runner

--- src/AnimationRunners.ts
+++ src/AnimationRunners.ts
@@ -1,10 +1,11 @@
-import Animated, { Easing } from "react-native-reanimated";
+import Animated, { Easing } from 'react-native-reanimated'
 
 const {
   Clock,
   Value,
   block,
   cond,
+  eq,
   stopClock,
   set,
   startClock,
@@ -13,66 +14,62 @@ const {
   and,
   timing: reTiming,
   decay: reDecay,
-  spring: reSpring
-} = Animated;
+  spring: reSpring,
+} = Animated
 
 interface AnimateParams<S, C> {
-  clock: Animated.Clock;
-  fn: (
-    clock: Animated.Clock,
-    state: S,
-    config: C
-  ) => Animated.Adaptable<number>;
-  state: S;
-  config: C;
-  from: Animated.Adaptable<number>;
+  clock: Animated.Clock
+  fn: (clock: Animated.Clock, state: S, config: C) => Animated.Adaptable<number>
+  state: S
+  config: C
+  from: Animated.Adaptable<number>
 }
 
 interface TimingAnimation {
-  state: Animated.TimingState;
-  config: Animated.TimingConfig;
+  state: Animated.TimingState
+  config: Animated.TimingConfig
 }
 
 interface SpringAnimation {
-  state: Animated.SpringState;
-  config: Animated.SpringConfig;
+  state: Animated.SpringState
+  config: Animated.SpringConfig
 }
 
 interface DecayAnimation {
-  state: Animated.DecayState;
-  config: Animated.DecayConfig;
+  state: Animated.DecayState
+  config: Animated.DecayConfig
 }
 
-type Animation = SpringAnimation | DecayAnimation | TimingAnimation;
+type Animation = SpringAnimation | DecayAnimation | TimingAnimation
 
 const onInit = (clock: Animated.Clock, sequence: Animated.Adaptable<number>) =>
-  cond(not(clockRunning(clock)), sequence);
+  cond(not(clockRunning(clock)), sequence)
 
 const animate = <T extends Animation>({
   fn,
   clock,
   state,
   config,
-  from
-}: AnimateParams<T["state"], T["config"]>) =>
+  from,
+}: AnimateParams<T['state'], T['config']>) =>
   block([
     onInit(clock, [
       set(state.finished, 0),
       set(state.time, 0),
       set(state.position, from),
-      startClock(clock)
+      startClock(clock),
     ]),
     fn(clock, state, config),
     cond(state.finished, stopClock(clock)),
-    state.position
-  ]);
+    state.position,
+  ])
 
 export interface TimingParams {
-  clock?: Animated.Clock;
-  from?: Animated.Adaptable<number>;
-  to?: Animated.Adaptable<number>;
-  duration?: Animated.Adaptable<number>;
-  easing?: Animated.EasingFunction;
+  clock?: Animated.Clock
+  from?: Animated.Adaptable<number>
+  to?: Animated.Adaptable<number>
+  duration?: Animated.Adaptable<number>
+  easing?: Animated.EasingFunction
 }
 
 export const timing = (params: TimingParams) => {
@@ -82,21 +79,21 @@ export const timing = (params: TimingParams) => {
     duration: 250,
     from: 0,
     to: 1,
-    ...params
-  };
+    ...params,
+  }
 
   const state: Animated.TimingState = {
     finished: new Value(0),
     position: new Value(0),
     time: new Value(0),
-    frameTime: new Value(0)
-  };
+    frameTime: new Value(0),
+  }
 
   const config = {
     toValue: new Value(0),
     duration,
-    easing
-  };
+    easing,
+  }
 
   return block([
     onInit(clock, [set(config.toValue, to), set(state.frameTime, 0)]),
@@ -105,16 +102,16 @@ export const timing = (params: TimingParams) => {
       fn: reTiming,
       state,
       config,
-      from
-    })
-  ]);
-};
+      from,
+    }),
+  ])
+}
 
 export interface DecayParams {
-  clock?: Animated.Clock;
-  from?: Animated.Adaptable<number>;
-  velocity?: Animated.Value<number>;
-  deceleration?: Animated.Adaptable<number>;
+  clock?: Animated.Clock
+  from?: Animated.Adaptable<number>
+  velocity?: Animated.Value<number>
+  deceleration?: Animated.Adaptable<number>
 }
 
 export const decay = (params: DecayParams) => {
@@ -123,19 +120,19 @@ export const decay = (params: DecayParams) => {
     velocity: new Value(0),
     deceleration: 0.998,
     from: 0,
-    ...params
-  };
+    ...params,
+  }
 
   const state: Animated.DecayState = {
     finished: new Value(0),
     position: new Value(0),
     time: new Value(0),
-    velocity: new Value(0)
-  };
+    velocity: new Value(0),
+  }
 
   const config: Animated.DecayConfig = {
-    deceleration
-  };
+    deceleration,
+  }
 
   return block([
     onInit(clock, [set(state.velocity, velocity)]),
@@ -144,19 +141,19 @@ export const decay = (params: DecayParams) => {
       fn: reDecay,
       state,
       config,
-      from
-    })
-  ]);
-};
+      from,
+    }),
+  ])
+}
 
-export type SpringConfig = Omit<Animated.SpringConfig, "toValue">;
+export type SpringConfig = Omit<Animated.SpringConfig, 'toValue'>
 
 export interface SpringParams {
-  clock?: Animated.Clock;
-  from?: Animated.Adaptable<number>;
-  to: Animated.Adaptable<number>;
-  velocity?: Animated.Value<number>;
-  config?: SpringConfig;
+  clock?: Animated.Clock
+  from?: Animated.Adaptable<number>
+  to: Animated.Adaptable<number>
+  velocity?: Animated.Value<number>
+  config?: SpringConfig
 }
 
 export const spring = (params: SpringParams) => {
@@ -164,15 +161,15 @@ export const spring = (params: SpringParams) => {
     clock: new Clock(),
     velocity: new Value(0),
     from: 0,
-    ...params
-  };
+    ...params,
+  }
 
   const state: Animated.SpringState = {
     finished: new Value(0),
     position: new Value(0),
     time: new Value(0),
-    velocity: new Value(0)
-  };
+    velocity: new Value(0),
+  }
 
   const config = {
     toValue: new Value(0),
@@ -182,8 +179,8 @@ export const spring = (params: SpringParams) => {
     overshootClamping: false,
     restSpeedThreshold: 0.01,
     restDisplacementThreshold: 0.01,
-    ...springConfig
-  };
+    ...springConfig,
+  }
 
   return block([
     onInit(clock, [set(config.toValue, to), set(state.velocity, velocity)]),
@@ -192,25 +189,22 @@ export const spring = (params: SpringParams) => {
       fn: reSpring,
       state,
       config,
-      from
-    })
-  ]);
-};
+      from,
+    }),
+  ])
+}
 
 export const delay = (node: Animated.Node<number>, duration: number) => {
-  const clock = new Clock();
-  return block([
-    timing({ clock, from: 0, to: 1, duration }),
-    cond(not(clockRunning(clock)), node)
-  ]);
-};
+  const clock = new Clock()
+  return block([timing({ clock, from: 0, to: 1, duration }), cond(not(clockRunning(clock)), node)])
+}
 
 export interface LoopProps {
-  clock?: Animated.Clock;
-  easing?: Animated.EasingFunction;
-  duration?: number;
-  boomerang?: boolean;
-  autoStart?: boolean;
+  clock?: Animated.Clock
+  easing?: Animated.EasingFunction
+  duration?: number
+  boomerang?: boolean
+  autoStart?: boolean
 }
 
 export const loop = (loopConfig: LoopProps) => {
@@ -220,19 +214,19 @@ export const loop = (loopConfig: LoopProps) => {
     duration: 250,
     boomerang: false,
     autoStart: true,
-    ...loopConfig
-  };
+    ...loopConfig,
+  }
   const state = {
     finished: new Value(0),
     position: new Value(0),
     time: new Value(0),
-    frameTime: new Value(0)
-  };
+    frameTime: new Value(0),
+  }
   const config = {
     toValue: new Value(1),
     duration,
-    easing
-  };
+    easing,
+  }
 
   return block([
     cond(and(not(clockRunning(clock)), autoStart ? 1 : 0), startClock(clock)),
@@ -241,10 +235,57 @@ export const loop = (loopConfig: LoopProps) => {
       set(state.finished, 0),
       set(state.time, 0),
       set(state.frameTime, 0),
-      boomerang
-        ? set(config.toValue, cond(config.toValue, 0, 1))
-        : set(state.position, 0)
+      boomerang ? set(config.toValue, cond(config.toValue, 0, 1)) : set(state.position, 0),
     ]),
-    state.position
-  ]);
-};
+    state.position,
+  ])
+}
+
+export const toggle = (
+  params: TimingParams & {
+    closeState: Animated.Value<number>
+    openState: Animated.Value<number>
+    value: Animated.Value<number>
+  },
+) => {
+  const { clock, value, closeState, openState, easing, duration, from, to } = {
+    clock: new Clock(),
+    easing: Easing.linear,
+    duration: 250,
+    from: 0,
+    to: 1,
+    closeState: 0,
+    openState: 0,
+    value: new Value(0),
+    ...params,
+  }
+
+  return [
+    cond(eq(openState, 1), [
+      set(
+        value,
+        timing({
+          clock,
+          from: value,
+          to,
+          easing,
+          duration,
+        }),
+      ),
+      cond(not(clockRunning(clock)), [set(openState, 0)]),
+    ]),
+    cond(eq(closeState, 1), [
+      set(
+        value,
+        timing({
+          clock,
+          from: value,
+          to: from,
+          easing,
+          duration,
+        }),
+      ),
+      cond(not(clockRunning(clock)), [set(closeState, 0)]),
+    ]),
+  ]
+}
